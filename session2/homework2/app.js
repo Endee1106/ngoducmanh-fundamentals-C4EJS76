608b9ba2050e122c8c20a14dcff845808e2fb6d0
Endee1106 <endee1106@gmail.com>
2020-04-03 12:54:47 +0700

    Finish ss2 homework

---
+++ session2/homework2/app.js
@@ -0,0 +1,103 @@
+// Bài 1
+
+// let x = prompt("Nhập số bất kì")
+
+// if ( Number(x) < 0) {
+//     console.log("invaild input");
+    
+// } else {
+//    let r = 1;
+//    for (let i=0; i< Number(x);i++)
+// {
+//     r=r*(i+1);
+
+// }
+//     console.log(r);
+    
+    
+// }
+
+
+// Bài 2 
+
+// let name = prompt("Nhap ten");
+// let age  = prompt("Nhap tuoi");
+    
+//     if ( Number(age)<18 ) {
+
+//     console.log(name + " chua du tuoi de vao trang web" );
+    
+//     }
+//     else {
+        
+//         let a = prompt("Ban co muon dang nhap");
+//         if ( a === "yes"){
+//             console.log(name + " da dang nhap");
+            
+//         }
+//         else { 
+//             console.log((name + " da thoat dang nhap"));
+            
+//         }
+//     }
+
+
+
+//  Bai 3 
+
+// let username = prompt("Nhap username");
+// let password = prompt("Nhap password");
+
+// let username2 = "ngoducmanh";
+// let password2 = "manhducngo";
+
+// if (username===username2 && password===password2)
+// {
+//     console.log("dang nhap thanh cong");
+    
+// }
+// else if (username===username2 && password!==password2)
+// {
+//     console.log("wrong password");
+    
+// }
+// else {
+//     console.log("wrong username");
+    
+// }
+
+
+
+// Bài 4 
+
+// let nameinput = prompt("nhap ten");
+// let monthinput = prompt("Nhap thang sinh")
+// switch (Number(monthinput)) {
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log(nameinput + " sinh vào mùa xuân");
+//         break;
+//     case 4:
+//     case 5:
+//     case 6:
+//         console.log(nameinput + " sinh vào mùa hạ");
+//         break;
+//     case 7:
+//     case 8:
+//     case 9:
+//         console.log(nameinput + " sinh vào mùa thu");
+//         break;
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log(nameinput + " sinh vào mùa đông");
+//         break;
+//     default:
+//         console.log("input không hợp lệ");
+//         break;
+// }
+
+
+
+// Bài 5 bài 6 em chịu :((
