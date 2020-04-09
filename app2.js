f8fb24b49641fbc1abba4f34b4e48a1b86a51ecf
Endee1106 <62002507+Endee1106@users.noreply.github.com>
2020-04-09 21:53:56 +0700

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,66 @@
+// Bài 1
+
+// let inputNumber = Number(prompt("Nhập số N"));
+// let tongsole = 0 
+// let tongsochan = 0
+// for (i = 1 ; i <= inputNumber ; i++)
+// { 
+//     if ( i % 2 == 0 ) {
+// tongsochan+=i ;
+//     }
+//     else {
+//         tongsole+=i;
+//     }
+// }
+// console.log("tổng các số chắn = "+ tongsochan);
+// console.log("tổng các số lẻ = "+ tongsole);
+
+// Bài 2
+
+// let n = Number(prompt("Nhap so ban muon"))
+// let flag = true
+// if(n<2){
+//     flag = false
+// }else{
+//     for(i=2;i< n-1;i++){
+//         if( n%i==0){
+//             flag = false;
+//             break;
+//         }
+//     }
+// }
+
+
+
+// if (flag=true){
+//     document.write(n + " la so nguyen to");
+    
+// }else {
+// document.write  (n +" khong la so nguyen to");
+// }
+
+// Bai 3
+
+// let userNumber = Number(prompt("Nhập vào số muốn in ra các số nguyên tố nhỏ hơn"));
+// if (userNumber < 2){
+//     console.log(`Không có số nguyên tố nào nhỏ hơn ${userNumber}`)
+// }else{
+//     console.log(`Số nguyên tố nhỏ hơn hoặc bằng ${userNumber} là :`)
+//     for (let i = 2; i <= userNumber; i++) {
+//         let count = 0;
+//         for (let j = 1 ; j <= i; j++) {
+//             if (i % j === 0) {
+//                 count++;
+//             }
+//         }
+//         if (count === 2) {
+//             console.log(i);
+//         }
+//     }
+// 
+
+// bai 4
+// bai 5
+
+
+
